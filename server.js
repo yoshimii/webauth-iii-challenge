862e88c0540d80b1f69527027cedd1b42391db6c
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-16 16:48:17 -0500

    users router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const server = express()
+const server = express();
+const usersRouter = require('./api/user-router.js');
 
 server.use(express.json());
-
+server.use('/api', usersRouter)
 module.exports = server;
